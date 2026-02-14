ee7df729ecec834e1c2251d4667be29d504951e9
Fahad Manoly <fahadmanoly@gmail.com>
2026-02-14 21:02:18 +0530

    Set up API for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,26 @@
+// // import axios from 'axios';
+
+// // const API = axios.create({
+// //     baseURL: 'http://127.0.0.1:8000/api/',
+// // });
+
+// // export const getEmployees = () => API.get('employees/');
+
+
+// import axios from 'axios';
+
+// const API = axios.create({
+//     baseURL: 'https://hrms-lite-lf5q.onrender.com/api/',
+// });
+
+// export const getEmployees = () => API.get('employees/');
+
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
+
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: BASE_URL,
 });
 
 export const getEmployees = () => API.get('employees/');
-
